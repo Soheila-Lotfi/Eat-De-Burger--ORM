3c4e27a7637811fe1ee1657a618ab05927298c66
soheila <soheilalotfi@ymail.com>
2019-09-02 20:35:48 -0400

    create schema and seeds.sql

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES (Cheese burger, false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES (Chicken burger, false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES (Ham burger, false);
